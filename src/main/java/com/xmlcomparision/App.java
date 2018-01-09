cc4284a0d4b064281814d4bf51b0428662202ba4
Naveen Kumar Reddy Kankanala <naveenkankanala9@gmail.com>
2018-01-08 20:02:13 -0500

    First commit

---
+++ src/main/java/com/xmlcomparision/App.java
@@ -0,0 +1,13 @@
+package com.xmlcomparision;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
